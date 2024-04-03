b4930483c72b8eaed04bac3a238694707f916d6c
Lionel Barret <lionel.barret@gmail.com>
2024-04-02 23:45:22 -0400

    adding a few commands.  nerd font

--- init.lua
+++ init.lua
@@ -91,7 +91,8 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
+vim.opt.guifont = 'JetBrainsMono Nerd Font Mono:h9'
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -859,6 +860,43 @@ require('lazy').setup({
     },
   },
 })
+
+-- go to Dragon folder
+vim.keymap.set('n', '<F12>', function()
+  vim.loop.chdir 'D:/perforce/Dragon/Dragon/Source'
+  print('moved to ' .. vim.loop.cwd())
+end)
+
+-- go to scraches / note
+vim.keymap.set('n', '<F2>', function()
+  local f = vim.env.HOME .. '/Documents/Scratch'
+  print(f)
+  vim.loop.chdir(f)
+  print('moved to ' .. vim.loop.cwd())
+end)
+
+-- open journal
+vim.keymap.set('n', '<F10>', function()
+  local f = vim.env.HOME .. '/Documents/Scratch'
+  vim.cmd.edit(f .. '/journal.md')
+end)
+
+function write_current_date_to_buffer()
+  -- Get the current date in YYYY-MM-DD format
+  local date = os.date '%Y-%m-%d'
+  -- Convert the date string into a table of lines
+  local lines = { '# ' .. date }
+  -- To get the current buffer in Lua within Neovim, you can use the
+  local buf = vim.api.nvim_get_current_buf()
+  -- Write the lines to the buffer
+  vim.api.nvim_buf_set_lines(buf, 0, 0, true, lines)
+end
+
+-- open neovim config
+vim.keymap.set('n', '<F11>', function()
+  local f = vim.fn.stdpath 'config'
+  vim.cmd.edit(f .. '/init.lua')
+end)
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
